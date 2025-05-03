88a68f3214d0ef708bfb2ded1c91358bb7afc5ac
Adilla-rgp <adillarobertagomespereira@gmail.com>
2025-05-03 14:13:39 -0300

    Adiciona estrutura inicial do projeto, incluindo fonte (src), Makefile para compilação e executável.

--- src/colecao.c
+++ src/colecao.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include "colecao.h"
 
-Colecao* criarColecao(int capacidade){
-    Colecao *nova = (int*)malloc(sizeof(Colecao)); 
-    if(nova == NULL){
+Colecao* criarColecao(int capacidade) {
+    Colecao *nova = (Colecao*)malloc(sizeof(Colecao)); 
+    if (nova == NULL) {
         return NULL; 
     }
-    nova->discos = (Disco*)malloc(sizeof(Disco));
-    if(nova->discos == NULL){
+    
+    nova->discos = (Disco*)malloc(capacidade * sizeof(Disco));
+    if (nova->discos == NULL) {
+        free(nova); // Libera a memória alocada para evitar vazamento
         return NULL; 
     }
+
     nova->capacidade = capacidade;
     nova->tamanho = 0; 
+
+    return nova;
 }
